fdb9fc8a628ea33caf573815e449518179a72ce3
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-02-17 00:00:30 +1030

    Ensure consumeParams leaves rest empty

--- params_test.go
+++ params_test.go
@@ -12,8 +12,9 @@ func TestParamEncoding(t *testing.T) {
 
 	assert.NoError(t, quick.Check(func(time, memory uint32, threads uint8) bool {
 		buf := appendParams(nil, time, memory, threads)
-		time2, memory2, threads2, _ := consumeParams(buf)
-		return time == time2 && memory == memory2 && threads == threads2
+		time2, memory2, threads2, rest := consumeParams(buf)
+		return time == time2 && memory == memory2 &&
+			threads == threads2 && len(rest) == 0
 	}, &quick.Config{
 		MaxCountScale: 10000,
 	}))
