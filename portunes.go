ef689c598b4490f66a6d3b309d019d948e38d244
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-02-22 18:01:41 +1030

    Improve package doc comment

--- portunes.go
+++ portunes.go
@@ -1,4 +1,5 @@
-// Package portunes password hashing daemon accessible via gRPC.
+// Package portunes provides a password hashing client and server
+// accessible via gRPC.
 package portunes
 
 //go:generate protoc ./portunes.proto --go_out=plugins=grpc:internal/proto
